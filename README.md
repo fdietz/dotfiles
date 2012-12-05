5c2d8612867f0cea215b86bc9c50f25c072f5431
Frederik Dietz <frederik.dietz@xing.com>
2012-12-05 16:51:47 +0100

    use bash install script instead of ruby Rakefile

---
+++ README.md
@@ -0,0 +1,19 @@
+# fdietz dotfiles
+
+These are config files to set up a system the way I like it.
+
+## Installation
+Copy and paste the following line into your terminal:
+
+    curl https://raw.github.com/fdietz/dotfiles/install.sh -L -o - | sh
+
+Or manually clone dotfiles and run the installer script:
+
+    git clone https://github.com/fdietz/dotfiles.git ~/.dotfiles
+    cd ~/.dotfiles
+    ./install.sh
+
+## OS X
+Use the osx script:
+
+    ./osx
---
+++ install.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/env sh
+
+dotfiles="$HOME/.dotfiles2"
+
+# to error out
+warn() {
+  echo "$1" >&2
+}
+
+die() {
+  warn "$1"
+  exit 1
+}
+
+lnif() {
+  if [ ! -e $2 ] ; then
+    ln -s $1 $2
+  fi
+}
+
+echo "Installing/Updating dotfiles...\n"
+
+if [ ! -e $dotfiles/.git ]; then
+  echo "Cloning dotfiles\n"
+  git clone https://github.com/fdietz/dotfiles.git $dotfiles
+else
+  echo "Updating dotfiles\n"
+  cd $dotfiles && git pull
+fi
+
+# bash
+echo "Setting up bash...\n"
+lnif $dotfiles/bash_profile $HOME/.bash_profile
+lnif $dotfiles/bashrc $HOME/.bashrc
+lnif $dotfiles/bash $HOME/.bash
+
+# ruby
+echo "Setting up dev tools...\n"
+lnif $dotfiles/ackrc $HOME/.ackrc
+lnif $dotfiles/ctags $HOME/.ctags
+lnif $dotfiles/gemrc $HOME/.gemrc
+lnif $dotfiles/gitconfig $HOME/.gitconfig
+lnif $dotfiles/gitignore_global $HOME/.gitignoreglobal
+lnif $dotfiles/irbrc $HOME/.irbirc
+lnif $dotfiles/rdebugrc $HOME/.rdebugrc
+
+# vim
+echo "Setting up vim...\n"
+lnif $dotfiles/vimrc $HOME/.vimrc
+lnif $dotfiles/gvimrc $HOME/.gvimrc
+lnif $dotfiles/vimrc.bundles $HOME/.vimrc.bundles
+lnif $dotfiles/vim $HOME/.vim
+
+# vim vundle
+if [ ! -d $dotfiles/vim/bundle ]; then
+    mkdir -p $dotfiles/vim/bundle
+fi
+
+if [ ! -e $dotfiles/vim/bundle/vundle ]; then
+  echo "Installing vundle"
+  git clone http://github.com/gmarik/vundle.git $dotfiles/vim/bundle/vundle
+fi
+
+echo "Update/Install plugins using vundle"
+vim -u $dotfiles/vimrc.bundles +BundleInstall! +BundleClean +qall
