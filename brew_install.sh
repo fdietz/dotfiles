855594e5c4e4aec9ee61f4b4f5eba22ab2638779
Frederik Dietz <fdietz@gmail.com>
2015-08-18 18:41:35 +0200

    complete brew install and casks support

---
+++ brew_install.sh
@@ -0,0 +1,135 @@
+#!/bin/sh
+
+fancy_echo() {
+  local fmt="$1"; shift
+
+  # shellcheck disable=SC2059
+  printf "\n$fmt\n" "$@"
+}
+
+brew_tap() {
+  brew tap "$1" 2> /dev/null
+}
+
+brew_install_or_upgrade() {
+  if brew_is_installed "$1"; then
+    if brew_is_upgradable "$1"; then
+      fancy_echo "Upgrading %s ..." "$1"
+      brew upgrade "$@"
+    else
+      fancy_echo "Already using the latest version of %s. Skipping ..." "$1"
+    fi
+  else
+    fancy_echo "Installing %s ..." "$1"
+    brew install "$@"
+  fi
+}
+
+brew_is_installed() {
+  local name="$(brew_expand_alias "$1")"
+
+  brew list -1 | grep -Fqx "$name"
+}
+
+brew_is_upgradable() {
+  local name="$(brew_expand_alias "$1")"
+
+  ! brew outdated --quiet "$name" >/dev/null
+}
+
+brew_expand_alias() {
+  brew info "$1" 2>/dev/null | head -1 | awk '{gsub(/:/, ""); print $1}'
+}
+
+brew_launchctl_restart() {
+  local name="$(brew_expand_alias "$1")"
+  local domain="homebrew.mxcl.$name"
+  local plist="$domain.plist"
+
+  fancy_echo "Restarting %s ..." "$1"
+  mkdir -p "$HOME/Library/LaunchAgents"
+  ln -sfv "/usr/local/opt/$name/$plist" "$HOME/Library/LaunchAgents"
+
+  if launchctl list | grep -Fq "$domain"; then
+    launchctl unload "$HOME/Library/LaunchAgents/$plist" >/dev/null
+  fi
+  launchctl load "$HOME/Library/LaunchAgents/$plist" >/dev/null
+}
+
+gem_install_or_update() {
+  if gem list "$1" --installed > /dev/null; then
+    fancy_echo "Updating %s ..." "$1"
+    gem update "$@"
+  else
+    fancy_echo "Installing %s ..." "$1"
+    gem install "$@"
+    rbenv rehash
+  fi
+}
+
+### here comes the installation
+
+if ! command -v brew >/dev/null; then
+  fancy_echo "Installing Homebrew ..."
+    curl -fsS \
+      'https://raw.githubusercontent.com/Homebrew/install/master/install' | ruby
+else
+  fancy_echo "Homebrew already installed. Skipping ..."
+fi
+
+fancy_echo "Updating Homebrew formulas ..."
+brew update
+
+brew_install_or_upgrade 'git'
+
+brew_install_or_upgrade 'postgres'
+brew_launchctl_restart 'postgresql'
+
+brew_install_or_upgrade 'the_silver_searcher'
+brew_install_or_upgrade 'vim'
+
+brew_install_or_upgrade 'hub'
+
+brew_install_or_upgrade 'node'
+
+brew_install_or_upgrade 'rbenv'
+brew_install_or_upgrade 'ruby-build'
+
+brew_install_or_upgrade 'openssl'
+brew unlink openssl && brew link openssl --force
+
+ruby_version="2.2.2"
+
+eval "$(rbenv init -)"
+
+if ! rbenv versions | grep -Fq "$ruby_version"; then
+  rbenv install -s "$ruby_version"
+fi
+
+rbenv global "$ruby_version"
+rbenv shell "$ruby_version"
+
+gem update --system
+
+gem_install_or_update 'bundler'
+
+fancy_echo "Configuring Bundler ..."
+  number_of_cores=$(sysctl -n hw.ncpu)
+  bundle config --global jobs $((number_of_cores - 1))
+
+brew_install_or_upgrade 'heroku-toolbelt'
+
+# optional apps
+
+# brew_tap 'caskroom/cask'
+# brew_install_or_upgrade 'brew-cask'
+
+# brew cask install dropbox
+# brew cask install google-chrome
+# brew cask install firefox
+# brew cask install spotify
+# brew cask install sequel-pro
+# brew cask install psequel
+# brew cask install atom
+# brew cask install iterm2
+# brew cask install sourcetree
