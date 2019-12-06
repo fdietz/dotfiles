f8546f809fd5c0636981d58a867965013efbd252
Frederik Dietz <fdietz@gmail.com>
2019-12-06 16:53:58 +0100

    chore: add yarn install

--- apt_install.sh
+++ apt_install.sh
@@ -16,20 +16,34 @@ function install {
 # dev tools required for installed languages
 install automake
 install autoconf
-install libreadline-dev
 install libncurses-dev
 install libssl-dev
-install libyaml-dev
 install libxslt-dev
 install libffi-dev
 install libtool
 install unixodbc-dev
 install unzip
 install curl
+install git-core
+install zlib1g-dev
+install build-essential
+install libssl-dev
+install libreadline-dev
+install libyaml-dev
+install libsqlite3-dev
+install sqlite3
+install libxml2-dev
+install libxslt1-dev
+install libcurl4-openssl-dev
+install software-properties-common
+install libffi-dev
+install libpq-dev
 
 # other
 install git
 install gnupg2
 install ripgrep
 install vim
+install yarn
 install firefox
+install postgresql-11
